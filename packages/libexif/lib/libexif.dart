a51fe1153a2b647d46278a85550fff6352b4a54c
Todd Volkert <tvolkert@gmail.com>
2023-11-16 22:48:32 -0800

    Initial commit

---
+++ packages/libexif/lib/libexif.dart
@@ -0,0 +1,8 @@
+/// Dart FFI bindings for https://github.com/libexif/libexif
+///
+/// See also:
+///
+///  * https://libexif.github.io/api/index.html
+library libexif;
+
+export 'src/third_party/libexif.dart';
---
+++ packages/libjpeg/lib/libjpeg.dart
@@ -0,0 +1,4 @@
+/// Dart FFI bindings for https://github.com/libexif/exif/tree/master/libjpeg
+library libjpeg;
+
+export 'src/third_party/libjpeg.dart';
